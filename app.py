7e8c709832ef9669574f4734424722776c93e218
Lam Van <tienlam2001@gmail.com>
2025-06-04 11:40:16 -0400

    sdf

--- app.py
+++ app.py
@@ -363,4 +363,5 @@ def download_pdf():
     return send_file(buffer, mimetype='application/pdf', as_attachment=True, download_name=f"{title}.pdf")
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=10000)
+    import os
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
